58ff641b84344bc25ea8ef13b1a1d611235f2cb3
roman-kozachenko <roman.kozachenko@gmail.com>
2016-05-04 22:50:34 +0300

    Project version update to 0.5.0. Change-version script removed.

--- scripts/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-source ~/.dnx/dnvm/dnvm.sh
-dnu restore
-dnu build src/msgpack.light && dnu build tests/msgpack.light.tests
-dnx -p tests/msgpack.light.tests test -parallel none
