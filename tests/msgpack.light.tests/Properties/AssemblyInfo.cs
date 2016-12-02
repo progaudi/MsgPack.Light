af199ce0a8b2e4b081eed4407d5ed453d64eccb1
Anatoly Popov <aensidhe@users.noreply.github.com>
2016-12-02 19:16:03 +0300

    Port tests projects

--- tests/msgpack.light.tests/Properties/AssemblyInfo.cs
+++ tests/msgpack.light.tests/Properties/AssemblyInfo.cs
@@ -5,10 +5,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("MsgPack.Light.Tests")]
-[assembly: AssemblyProduct("MsgPack.Light")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
