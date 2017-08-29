f1c86f47622ae7c5a8cc2329b9afc47c5f3e86ab
Anatoly Popov <aensidhe@users.noreply.github.com>
2017-08-29 14:51:09 +0300

    Build scripts for travis fixed.

--- scripts/build-netcore.sh
+++ scripts/build-netcore.sh
@@ -7,8 +7,6 @@ pushd ${BASH_SOURCE%/*}
 cd ..
 
 dotnet restore
-dotnet build -c Release -f netstandard1.1 src/msgpack.light/msgpack.light.csproj
-dotnet build -c Release -f netcoreapp1.0 tests/msgpack.light.tests/msgpack.light.tests.csproj
-dotnet build -c Release -f netcoreapp1.1 tests/msgpack.light.tests/msgpack.light.tests.csproj
+dotnet build -c Release msgpack.sln
 
 popd
