dacbf8fd6ee7d677422a011d1a3641ce2f4ad35f
Anatoly Popov <me@aensidhe.ru>
2018-11-16 13:58:33 +0300

    Add TimeSpan format and parsing capabilities, remove unused code

--- src/msgpack.light/Converters/Date/Ticks.cs
+++ src/msgpack.light/Converters/Date/Ticks.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace ProGaudi.MsgPack.Converters.Date
 {
-    public class Ticks : IMsgPackFormatter<DateTime>, IMsgPackFormatter<DateTimeOffset>, IMsgPackParser<DateTime>, IMsgPackParser<DateTimeOffset>
+    public class Ticks : IMsgPackFormatter<DateTime>, IMsgPackFormatter<DateTimeOffset>, IMsgPackFormatter<TimeSpan>, IMsgPackParser<DateTime>, IMsgPackParser<DateTimeOffset>, IMsgPackParser<TimeSpan>
     {
         private static readonly int BufferSize = DataLengths.GetMinAndMaxLength(DataCodes.Int64).max;
 
@@ -12,12 +12,18 @@ namespace ProGaudi.MsgPack.Converters.Date
 
         int IMsgPackFormatter<DateTimeOffset>.GetBufferSize(DateTimeOffset value) => BufferSize;
 
+        int IMsgPackFormatter<TimeSpan>.GetBufferSize(TimeSpan value) => BufferSize;
+
         int IMsgPackFormatter<DateTimeOffset>.Format(Span<byte> destination, DateTimeOffset value) => MsgPackSpec.WriteInt64(destination, DateTimeUtils.FromDateTimeOffset(value));
 
         int IMsgPackFormatter<DateTime>.Format(Span<byte> destination, DateTime value) => MsgPackSpec.WriteInt64(destination, DateTimeUtils.FromDateTime(value));
 
+        int IMsgPackFormatter<TimeSpan>.Format(Span<byte> destination, TimeSpan value) => MsgPackSpec.WriteInt64(destination, value.Ticks);
+
         DateTime IMsgPackParser<DateTime>.Parse(ReadOnlySpan<byte> source, out int readSize) => DateTimeUtils.ToDateTime(MsgPackSpec.ReadInt32(source, out readSize));
 
         DateTimeOffset IMsgPackParser<DateTimeOffset>.Parse(ReadOnlySpan<byte> source, out int readSize) => DateTimeUtils.ToDateTimeOffset(MsgPackSpec.ReadInt32(source, out readSize));
+
+        TimeSpan IMsgPackParser<TimeSpan>.Parse(ReadOnlySpan<byte> source, out int readSize) => new TimeSpan(MsgPackSpec.ReadInt32(source, out readSize));
     }
 }
